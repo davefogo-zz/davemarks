55c3962276fdf93b8a46a59635966993ac03b8d7
David Forero <davefogo@gmail.com>
2016-07-19 14:10:59 -0500

    clean up topic index.html.erb and debug incoming_controller.rb

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,10 +2,6 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    # Take a look at these in your server logs
-    # to get a sense of what you're dealing with.
-    puts "INCOMING PARAMS HERE: #{params}"
-
     # You put the message-splitting and business
     # magic here.
 
