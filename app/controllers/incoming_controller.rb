60fcda68e74b699b52adcba30586fa5089eb80d1
David Forero <davefogo@gmail.com>
2016-08-09 12:47:11 -0500

    rename local variables for new user and topic

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -8,25 +8,25 @@ class IncomingController < ApplicationController
 
 
     # Find the user
-     user = User.find_by(email: params[:sender])
+     find_user = User.find_by(email: params[:sender])
      # Find the topic
-     topic = Topic.find_by(title: params[:subject])
+     find_topic = Topic.find_by(title: params[:subject])
      # Assign the url to a variable after retreiving it from
      url = params["body-plain"]
 
      if user.nil?
-       user = User.new(email: user, password: "password")
+       user = User.new(email: find_user, password: "password")
        user.save!
      end
 
      # If the topic is nil, create and save a new topic
 
       if topic.nil?
-        topic = Topic.new(title: topic, user: user)
+        topic = Topic.new(title: find_topic, user: find_user)
         topic.save!
       end
 
-      bookmark = topic.bookmarks.build(user: user, url: url)
+      bookmark = topic.bookmarks.build(user: find_user, url: url)
 
       bookmark.save!
 
