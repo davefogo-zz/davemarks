ea41e8792019a3844ca48ffea135062cf16371f3
David Forero <davefogo@gmail.com>
2016-08-09 12:59:33 -0500

    revert changes to previous commit

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,25 +4,25 @@ class IncomingController < ApplicationController
 
   def create
     # Find the user
-     find_user = User.find_by(email: params[:sender])
+     user = User.by(email: params[:sender])
      # Find the topic
-     find_topic = Topic.find_by(title: params[:subject])
+     topic = Topic.by(title: params[:subject])
      # Assign the url to a variable after retreiving it from
      url = params["body-plain"]
 
-     if find_user.nil?
-       user = User.new(email: find_user, password: "password")
+     if user.nil?
+       user = User.new(email: user, password: "password")
        user.save!
      end
 
      # If the topic is nil, create and save a new topic
 
-      if find_topic.nil?
-        topic = Topic.new(title: find_topic, user: find_user)
+      if topic.nil?
+        topic = Topic.new(title: topic, user: user)
         topic.save!
       end
 
-      bookmark = topic.bookmarks.build(user: find_user, url: url)
+      bookmark = topic.bookmarks.build(user: user, url: url)
 
       bookmark.save!
 
