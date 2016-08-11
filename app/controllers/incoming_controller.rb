0ced36fab6beb982ede5e8c539fc328018263d1d
David Forero <davefogo@gmail.com>
2016-08-11 17:46:45 -0500

    specify description for url in email parsing

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,23 +3,22 @@ class IncomingController < ApplicationController
   skip_before_action :authenticate_user!, only: [:create]
 
   def create
-Rails.logger.info "##############################"
-Rails.logger.info params.inspect
-
     # Find the user
      user = User.find_by(email: params[:sender])
+
      # Find the topic
      topic = Topic.find_by(title: params[:subject])
+
      # Assign the url to a variable after retreiving it from
      url = params["body-plain"]
-Rails.logger.info ">#{topic}<"
+
+     # If the user is nil, create and save a new user
      if user.nil?
-       user = User.new(email: user, password: "password")
+       user = User.new(email: params[:sender], password: "password")
        user.save!
      end
 
      # If the topic is nil, create and save a new topic
-
       if topic.nil?
         topic = Topic.new(title: params[:subject], user: user)
 
@@ -29,7 +28,6 @@ Rails.logger.info ">#{topic}<"
       bookmark = topic.bookmarks.build(user: user, url: url, description: "bookmark for #{url}")
 
       bookmark.save!
-
 
     # Assuming all went well.
     head 200
