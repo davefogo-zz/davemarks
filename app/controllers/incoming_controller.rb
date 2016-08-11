809d8dae26e821976bcb6dea2e839158264cf1b4
David Forero <davefogo@gmail.com>
2016-08-11 10:39:35 -0500

    fix bug in incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,15 +3,16 @@ class IncomingController < ApplicationController
   skip_before_action :authenticate_user!, only: [:create]
 
   def create
-    Rails.logger.info "##############################"
-    Rails.logger.info params.inspect
+Rails.logger.info "##############################"
+Rails.logger.info params.inspect
+
     # Find the user
      user = User.find_by(email: params[:sender])
      # Find the topic
      topic = Topic.find_by(title: params[:subject])
      # Assign the url to a variable after retreiving it from
      url = params["body-plain"]
-
+Rails.logger.info ">#{topic}<"
      if user.nil?
        user = User.new(email: user, password: "password")
        user.save!
@@ -20,7 +21,8 @@ class IncomingController < ApplicationController
      # If the topic is nil, create and save a new topic
 
       if topic.nil?
-        topic = Topic.new(title: topic, user: user)
+        topic = Topic.new(title: params[:subject], user: user)
+
         topic.save!
       end
 
