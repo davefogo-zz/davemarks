ea7760902f19098f945c20954cca7f9bbe896f6a
David Forero <davefogo@gmail.com>
2017-05-12 17:07:36 -0500

    remove confirmable module from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable
   has_many :topics
   has_many :bookmarks, dependent: :destroy
   has_many :likes, dependent: :destroy
