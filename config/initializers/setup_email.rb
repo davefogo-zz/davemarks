081a80acb0915ea8cea2fd29d45004fb0a98c43c
David Forero <davefogo@gmail.com>
2017-05-22 16:13:23 -0500

    set up mailgun

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -7,7 +7,7 @@ if Rails.env.development? || Rails.env.production?
     :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
     user_name:      ENV['MAILGUN_SMTP_LOGIN'],
     password:       ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:         'https://nameless-oasis-20950.herokuapp.com/',
+    domain:         'https://lit-citadel-94662.herokuapp.com/',
     enable_starttls_auto: true
   }
   ActionMailer::Base.raise_delivery_errors = true
