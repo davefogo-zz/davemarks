fea2906c0e44f283b2eb23733704e09ca2092c23
David Forero <davefogo@gmail.com>
2017-05-22 21:55:52 -0500

    modify default_url host in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'https://nameless-oasis-20950.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://lit-citadel-94662.herokuapp.com/' }
 end
